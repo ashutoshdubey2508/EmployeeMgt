4781f0c3e9aa439d1e265803e0651fbe76f0f6af
ashutoshdubey2508 <59065346+ashutoshdubey2508@users.noreply.github.com>
2023-08-07 07:16:21 +0530

    Delete src directory

--- src/main/java/com/notifications/microserviceproject/repository/EmployeeDescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.notifications.microserviceproject.repository;
-
-import com.notifications.microserviceproject.entity.EmployeeDescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeDescriptionRepository extends JpaRepository<EmployeeDescription, Integer> {
-}
